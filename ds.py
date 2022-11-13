8978002c61aaa4e4789d07b6d1e3cc5e4440baf2
JunYao <B10815044@gapps.ntust.edu.tw>
2022-11-13 23:59:34 +0800

    100%

--- ds.py
+++ ds.py
@@ -17,7 +17,7 @@ from lime import lime_tabular
 import csv
 import random
 from tqdm import tqdm
-
+import sys
 
 def utils_recognize_type(dtf, col, max_cat=20):
     if (dtf[col].dtype == "O") | (dtf[col].nunique() < 9):
@@ -28,17 +28,7 @@ def utils_recognize_type(dtf, col, max_cat=20):
 def inputPreprocess(dtf):
     
     dtf = dtf.drop("Attribute1", axis=1)
-    
-    dummy = pd.get_dummies(
-    dtf["Attribute8"],  prefix="Attribute8", drop_first=True)
-    dtf = pd.concat([dtf, dummy], axis=1)
-    dtf.loc[dtf.Attribute8.isnull(), dtf.columns.str.startswith("Attribute8_")] = np.nan
     dtf = dtf.drop("Attribute8", axis=1)
-
-    dummy = pd.get_dummies(
-    dtf["Attribute10"],  prefix="Attribute10", drop_first=True)
-    dtf = pd.concat([dtf, dummy], axis=1)
-    dtf.loc[dtf.Attribute10.isnull(), dtf.columns.str.startswith("Attribute10_")] = np.nan
     dtf = dtf.drop("Attribute10", axis=1)
 
     dummy = pd.get_dummies(
@@ -52,14 +42,6 @@ def inputPreprocess(dtf):
     dtf = pd.concat([dtf, dummy], axis=1)
     dtf.loc[dtf.Attribute16.isnull(), dtf.columns.str.startswith("Attribute16_")] = np.nan
     dtf = dtf.drop("Attribute16", axis=1)
-    
-    count = 0
-    while count <= 10827:
-        num = random.randrange(0,17104-count-1)
-        if(dtf.iloc[[num]].values[0][11]=="No"):
-            count = count+1
-            dtf = dtf.drop(num, axis=0)
-            dtf = dtf.reset_index(drop=True)
             
     return dtf
             
@@ -68,7 +50,7 @@ def trainPreprocess(dtf):
     dtf = dtf.rename(columns={"Attribute17": "Y"})
     dtf = dtf.replace({'Y': {'Yes': 1, 'No': 0}})
 
-    imputer = KNNImputer(n_neighbors=5, weights="uniform")
+    imputer = KNNImputer(n_neighbors=3, weights="uniform")
     
     for i in tqdm(dtf.columns[dtf.isnull().any()].tolist()):
         dtf[i] = imputer.fit_transform(dtf[[i]])
@@ -77,22 +59,9 @@ def trainPreprocess(dtf):
     
     dtf.to_csv('trainSet.csv')
 
-    
-    # dtf["Attribute3"] = dtf["Attribute3"].fillna(dtf["Attribute3"].mean())
-    # dtf["Attribute4"] = dtf["Attribute4"].fillna(dtf["Attribute4"].mean())
-    # dtf["Attribute5"] = dtf["Attribute5"].fillna(dtf["Attribute5"].mean())
-    # dtf["Attribute6"] = dtf["Attribute6"].fillna(dtf["Attribute6"].mean())
-    # dtf["Attribute7"] = dtf["Attribute7"].fillna(dtf["Attribute7"].mean())
-    # dtf["Attribute9"] = dtf["Attribute9"].fillna(dtf["Attribute9"].mean())
-    # dtf["Attribute11"] = dtf["Attribute11"].fillna(dtf["Attribute11"].mean())
-    # dtf["Attribute12"] = dtf["Attribute12"].fillna(dtf["Attribute12"].mean())
-    # dtf["Attribute13"] = dtf["Attribute13"].fillna(dtf["Attribute13"].mean())
-    # dtf["Attribute15"] = dtf["Attribute15"].fillna(dtf["Attribute15"].mean())
-    
     scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
     X = scaler.fit_transform(dtf.drop("Y", axis=1))
-    dtf_scaled = pd.DataFrame(X, columns=dtf.drop(
-        "Y", axis=1).columns, index=dtf.index)
+    dtf_scaled = pd.DataFrame(X, columns=dtf.drop("Y", axis=1).columns, index=dtf.index)
     dtf_scaled["Y"] = dtf["Y"]
     dtf = dtf_scaled
     
@@ -101,19 +70,13 @@ def trainPreprocess(dtf):
 def testPreprocess(dtf):
     
     dtf = dtf.drop("Attribute1", axis=1)
-    dtf = dtf.dropna(axis=0).reset_index(drop=True)
-    dummy = pd.get_dummies(
-    dtf["Attribute8"],  prefix="Attribute8", drop_first=True)
-    dtf = pd.concat([dtf, dummy], axis=1)
     dtf = dtf.drop("Attribute8", axis=1)
-    dummy = pd.get_dummies(dtf["Attribute10"],
-                        prefix="Attribute10", drop_first=True)
-    dtf = pd.concat([dtf, dummy], axis=1)
     dtf = dtf.drop("Attribute10", axis=1)
-    dummy = pd.get_dummies(dtf["Attribute14"],
-                        prefix="Attribute14", drop_first=True)
+
+    dummy = pd.get_dummies(dtf["Attribute14"],prefix="Attribute14", drop_first=True)
     dtf = pd.concat([dtf, dummy], axis=1)
     dtf = dtf.drop("Attribute14", axis=1)
+    
     dummy = pd.get_dummies(dtf["Attribute16"],
                         prefix="Attribute16", drop_first=True)
     dtf = pd.concat([dtf, dummy], axis=1)
@@ -124,47 +87,39 @@ def testPreprocess(dtf):
     
     return dtf
     
-    
+_dtf = pd.read_csv('train.csv') 
 
-dtf = inputPreprocess(pd.read_csv('train.csv'))
+Y = _dtf.loc[_dtf['Attribute17'] == "Yes"]
+N = _dtf.loc[_dtf['Attribute17'] == "No"]
+N = N.dropna(how='any')
 
+Y = trainPreprocess(inputPreprocess(Y))
+N = trainPreprocess(inputPreprocess(N))
 
+Y.to_csv('./data/inputProcessed/Attribute17_Y_train.csv',index=False)
+N.to_csv('./data/inputProcessed/Attribute17_N_train.csv',index=False)
 
-dtf.to_csv('Processed_train.csv')
+count = 0
+while count <= 2558:
+    num = random.randrange(0,5697-count-1)
+    count = count+1
+    N = N.drop(num, axis=0)
+    N = N.reset_index(drop=True)
 
+Y_more, Y_less = model_selection.train_test_split(Y, test_size=0.1)
+N_more, N_less = model_selection.train_test_split(N, test_size=0.1)
 
+trainSet = pd.concat([Y_more,N_more]);
+validSet = pd.concat([Y_less,N_less]);
 
+dtf_train = trainSet
+dtf_valid = validSet
 
-dtf_train, dtf_valid = model_selection.train_test_split(dtf, test_size=0.2)
-
-dtf_train = trainPreprocess(dtf)
-dtf_valid = trainPreprocess(dtf_valid)
+dtf_train = pd.read_csv('trainSet_0.9943334613605899_.csv') 
+dtf_valid = pd.read_csv('validSet_0.9943334613605899_.csv') 
 
 test_data = pd.read_csv('test.csv')
 test_data = testPreprocess(test_data)
-
-#dtf_valid = Preprocess(dtf_valid)
-
-
-# Data Encoding
-
-
-
-#dtf_valid.to_csv('Processed_test.csv')
-
-
-
-# heatmap = dtf.isnull()
-
-# for k,v in dic_cols.items():
-#  if v == "num":
-#    heatmap[k] = heatmap[k].apply(lambda x: 0.5 if x is False else 1)
-#  else:
-#    heatmap[k] = heatmap[k].apply(lambda x: 0 if x is False else 1)
-# sns.heatmap(heatmap, cbar=False).set_title('Dataset Overview')
-# plt.show()
-
-
 
 X = dtf_train.drop("Y", axis=1).values
 y = dtf_train["Y"].values
@@ -204,7 +159,7 @@ plt.grid(axis='both')
 results = []
 
 #for i in range(1,len(dtf_importances.index)):
-X_names = dtf_importances.index[:17]
+X_names = dtf_importances.index
 X_train = dtf_train[X_names].values
 y_train = dtf_train["Y"].values
 X_test = dtf_valid[X_names].values
@@ -215,19 +170,16 @@ y_test = dtf_valid["Y"].values
 # call model
 model = ensemble.GradientBoostingClassifier()
 ## define hyperparameters combinations to try
-param_dic = {'learning_rate':[0.01],      #weighting factor for the corrections by new trees when added to the model
-'n_estimators':[500],  #number of trees added to the model
-'max_depth':[2],    #maximum depth of the tree
-'min_samples_split':[4],    #sets the minimum number of samples to split
-'min_samples_leaf':[3],     #the minimum number of samples to form a leaf
-'max_features':[2],     #square root of features is usually a good starting point
-'subsample':[0.9]}       #the fraction of samples to be used for fitting the individual base learners. Values lower than 1 generally lead to a reduction of variance and an increase in bias.
-
+param_dic = {'learning_rate':[0.15],      #weighting factor for the corrections by new trees when added to the model
+'n_estimators':[1750],  #number of trees added to the model
+'max_depth':[3],    #maximum depth of the tree
+'min_samples_split':[8],    #sets the minimum number of samples to split
+'min_samples_leaf':[1],     #the minimum number of samples to form a leaf
+'max_features':[7],     #square root of features is usually a good starting point
+'subsample':[0.8]}       #the fraction of samples to be used for fitting the individual base learners. Values lower than 1 generally lead to a reduction of variance and an increase in bias.
 random_search = model_selection.RandomizedSearchCV(model,param_distributions=param_dic, n_iter=1000,scoring="accuracy", n_jobs = -1,verbose=1).fit(X_train, y_train)
 print("Best Model parameters:", random_search.best_params_)
 print("Best Model mean accuracy:", random_search.best_score_)
-
-print(random_search.best_params_['learning_rate'])
 
 model = random_search.best_estimator_
 #results.append([i,random_search.best_score_,param_dic['learning_rate'][0], param_dic['n_estimators'][0],param_dic['max_depth'][0],param_dic['min_samples_split'][0],param_dic['min_samples_leaf'][0],param_dic['max_features'][0],param_dic['subsample'][0]])
@@ -264,8 +216,6 @@ ax.set_yticklabels(labels=classes, rotation=0)
 plt.show()
 
 
-
-
 predicted_ = model.predict(test_data[X_names].values)
 
 ans = []
@@ -275,14 +225,12 @@ for index, item in enumerate(predicted_):
 keys = range(0, len(predicted_))
 df_subm = pd.DataFrame({'id': keys, 'ans': predicted_})
 df_subm['id'] = df_subm["id"].astype("string")
-df_subm.to_csv('ans.csv', index=False)
+df_subm.to_csv('./data/ans_' + str(random_search.best_score_) + '_.csv', index=False)
 
 with open('ans.csv', 'w', newline='') as outfile:
     writer = csv.writer(outfile)
     writer.writerow(['id', 'ans'])
     writer.writerows(ans)
     
-    
-    #writer.writerow(['i', 'accuracy', 'learning_rate', 'n_estimators', 'max_depth', 'min_samples_split', 'min_samples_leaf', 'max_features', 'subsample'])
-    #writer.writerows(results)
-
+trainSet.to_csv('./data/split/trainSet_'+ str(random_search.best_score_) + '_.csv',index=False)
+validSet.to_csv('./data/split/validSet_'+ str(random_search.best_score_) + '_.csv',index=False)
